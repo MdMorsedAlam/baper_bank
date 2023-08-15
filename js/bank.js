9eccf467845751645e35c8bc1e241199b709a645
Md Morsed Alam <mdmorsedalam68@gmail.com>
2023-08-15 14:57:09 +0600

    all Done

--- js/bank.js
+++ js/bank.js
@@ -2,23 +2,32 @@
      document.getElementById("depo-btn").addEventListener("click",function(){
         const depoInpt=document.querySelector("#depo-inpt");
         const depoValueString=depoInpt.value;
-        const inptValue=parseFloat(depoValueString);
+        const depoInptValue=parseFloat(depoValueString);
 
         const depoInptString=document.getElementById("deposit").innerText;
         const depoValue=parseFloat(depoInptString);
-        document.getElementById("deposit").innerText=depoValue+inptValue;
+        document.getElementById("deposit").innerText=depoValue+depoInptValue;
 
         const blValueString=document.getElementById("balance").innerText;
         const blValue=parseFloat(blValueString);
-        document.getElementById("balance").innerText=blValue+inptValue;
+        document.getElementById("balance").innerText=blValue+depoInptValue;
 
         depoInpt.value='';
     })
     // ---------Withdraw---------
     document.getElementById("wid-btn").addEventListener("click",function(){
         const widInpt=document.querySelector("#wid-inpt");
-        const widValue=widInpt.value;
-        document.getElementById("withdraw").innerText=widValue;
+        const widInptValueString=widInpt.value;
+        const widInptValue=parseFloat(widInptValueString);
+
+        const widValueString=document.getElementById("withdraw").innerText;
+        const widValue=parseFloat(widValueString);
+        document.getElementById("withdraw").innerText=widValue+widInptValue;
+
+        const blString=document.getElementById("balance").innerText;
+        const blVal=parseFloat(blString);
+        document.getElementById("balance").innerText=blVal-widInptValue;
+
         widInpt.value='';   
     
      
